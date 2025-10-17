6ada7f482f78d1289ae0340cf1cec49a9873efe3
Sala G 801 <salag801@cunadm.edu.local>
2025-10-16 20:35:46 -0500

    Aprendiendo PHP

---
+++ acercade.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hola desde PHP 😒";
+
+$miVariable = "Soy variable 1";
+
+$mivariable = "Soy variable 2";
+
+echo $miVariable;
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+ echo "Hola, mundo me llamo<br>";
+ echo "fulanito, y me gusta<br>";
+ echo "programar";
+ phpinfo();
+?>
